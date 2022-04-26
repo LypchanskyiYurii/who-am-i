0afd6e8bee131a104b44abe9f07362fe8e7df287
Yurii <yurii.lypchanskyi@gmail.com>
2022-04-26 17:14:43 +0300

    [LECHW-5] Gathering of Players' answers in parallel

--- src/main/java/com/eleks/academy/whoami/networking/server/ServerImpl.java
+++ src/main/java/com/eleks/academy/whoami/networking/server/ServerImpl.java
@@ -14,9 +14,9 @@ import com.eleks.academy.whoami.core.impl.RandomPlayer;
 
 public class ServerImpl implements Server {
 
-	private List<String> characters = List.of("Batman", "Superman");
-	private List<String> questions = List.of("Am i a human?", "Am i a character from a movie?");
-	private List<String> guessess = List.of("Batman", "Superman");
+	private List<String> characters = List.of("Batman", "Superman", "Tor", "Gladiator");
+	private List<String> questions = List.of("Am i a human?", "Am i a character from a movie? ");
+	private List<String> guessess = List.of("Batman", "Superman", "Tor", "Gladiator");
 
 	private RandomGame game = new RandomGame(characters);
 
