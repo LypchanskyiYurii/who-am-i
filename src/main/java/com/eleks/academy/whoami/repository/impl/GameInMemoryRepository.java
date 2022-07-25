d9ad7c3286a1fa052f2b9f2b1df0e7663c67aa6b
Yurii <yurii.lypchanskyi@gmail.com>
2022-07-25 15:40:26 +0300

    Fixed text

--- src/main/java/com/eleks/academy/whoami/repository/impl/GameInMemoryRepository.java
+++ src/main/java/com/eleks/academy/whoami/repository/impl/GameInMemoryRepository.java
@@ -24,7 +24,6 @@ public class GameInMemoryRepository implements GameRepository {
                 .collect(Collectors.toList());
     }
 
-
     @Override
     public PersistentGame save(PersistentGame game) {
         this.games.add(game);
